f19880c3f5a60bdd97652c00d34bacd9a9dc32cc
mattentosh <mattentosh1@gmail.com>
2024-07-18 12:45:42 +0800

    PortalGate Orientation Fixed v2

--- src/main/java/nether_fortress_forever/modid/block/custom/PortalGateBlock.java
+++ src/main/java/nether_fortress_forever/modid/block/custom/PortalGateBlock.java
@@ -2,13 +2,21 @@ package nether_fortress_forever.modid.block.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
+import net.minecraft.util.function.BooleanBiFunction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.stream.Stream;
 
 public class PortalGateBlock extends Block {
     public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
@@ -16,6 +24,7 @@ public class PortalGateBlock extends Block {
     public PortalGateBlock(Settings settings) {
         super(settings);
     }
+
 
 
     @Nullable
