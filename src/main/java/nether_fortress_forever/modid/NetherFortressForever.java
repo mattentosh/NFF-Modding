c047448e225ec4063d55b86e3e477e9cdcaf5f64
mattentosh <mattentosh1@gmail.com>
2024-08-14 19:21:25 +0800

    Dungeon Attempt 1

--- src/main/java/nether_fortress_forever/modid/NetherFortressForever.java
+++ src/main/java/nether_fortress_forever/modid/NetherFortressForever.java
@@ -3,8 +3,10 @@ package nether_fortress_forever.modid;
 import net.fabricmc.api.ModInitializer;
 
 import nether_fortress_forever.modid.block.ModBlocks;
+import nether_fortress_forever.modid.datagen.ModWorldGenerator;
 import nether_fortress_forever.modid.item.ModItemGroups;
 import nether_fortress_forever.modid.item.ModItems;
+import nether_fortress_forever.modid.world.dimension.ModDimensions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +21,8 @@ public class NetherFortressForever implements ModInitializer {
 		ModItemGroups.registerItemGroups();
 
 		ModItems.registerModItems();
+
+		ModDimensions.register();
 
 		ModBlocks.registerModBlocks();
 		LOGGER.info("Hello Fabric world!");
--- src/main/java/nether_fortress_forever/modid/NetherFortressForeverDataGenerator.java
+++ src/main/java/nether_fortress_forever/modid/NetherFortressForeverDataGenerator.java
@@ -2,10 +2,26 @@ package nether_fortress_forever.modid;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
+import nether_fortress_forever.modid.datagen.ModWorldGenerator;
+import nether_fortress_forever.modid.world.dimension.ModDimensions;
 
 public class NetherFortressForeverDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(ModWorldGenerator::new);
+
+
+	}
+
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder){
+		registryBuilder.addRegistry(RegistryKeys.DIMENSION_TYPE, ModDimensions::bootstrapType);
+
 
 	}
 }
+
